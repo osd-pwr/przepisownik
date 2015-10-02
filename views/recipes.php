f58d9b2d16c2f9dee3b42648b1076a3ee936dd2f
Paweł Abramowicz <pawelabrams@gmail.com>
2015-10-02 18:16:45 +0200

    Added some quite nice css to the app

--- views/recipes.php
+++ views/recipes.php
@@ -8,20 +8,23 @@
   <head>
     <meta charset="utf-8">
     <title>Wyniki wyszukiwania</title>
+    <link rel="stylesheet" href="/css/master.css" media="screen" title="no title" charset="utf-8">
   </head>
-  <body>
-    <p>No hej! Oto twe przepisy!</p>
-    <ul>
-    <?php
-      foreach ($recipes as $recipe) {
-        echo '
-          <li>
-            <a href="'.$recipe['url'].'">
-              <h4>'.$recipe['name'].'</h4>
-              <p>Brakuje '.$recipe['need'].' z '.$recipe['ingredients_count'].'</p>
-            </a>
-          </li>';
-      } ?>
-  </ul>
+  <body class="search">
+    <div class="main card">
+      <p>No hej! Oto twe przepisy!</p>
+      <ul>
+      <?php
+        foreach ($recipes as $recipe) {
+          echo '
+            <li>
+              <a href="'.$recipe['url'].'">
+                <h4>'.$recipe['name'].'</h4>
+                <p>Brakuje '.$recipe['need'].' z '.$recipe['ingredients_count'].'</p>
+              </a>
+            </li>';
+        } ?>
+      </ul>
+    </div>
   </body>
 </html>
